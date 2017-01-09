dfa251d4c12dad10b43531e231ae348f4a285a1a
Anteoy <anteoy@gmail.com>
2017-01-09 21:15:37 +0800

    java polymorphism

---
+++ src/main/java/com/anteoy/coreJava/polymorphism/Polymorphism.java
@@ -0,0 +1,26 @@
+package com.anteoy.coreJava.polymorphism;
+
+/**
+ * Created by zhoudazhuang
+ * Date: 17-1-9
+ * Time: 下午9:07
+ * Description :java识别是否为重复冲突函数，依靠的是函数名和参数，与返回值无关，如yy(int a)
+ */
+public class Polymorphism {
+    //注释代码编译不通过,即使参数都为空 不允许仅仅只有返回值不同的同名函数
+    /*String yy(int a){
+
+    }
+    int yy(int a){
+        return 1;
+    }*/
+    String yy(int a,String b){
+        return null;
+    }
+    int yy(int b,int c){
+        return 1;
+    }
+    int yy (int a,boolean b){
+        return 1;
+    }
+}

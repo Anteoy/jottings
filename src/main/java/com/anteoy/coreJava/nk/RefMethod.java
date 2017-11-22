96af56ecb0813c17c529830b4f58294a3641b160
anteoy <anteoy@gmail.com>
2017-11-22 10:06:40 +0800

    pom jdk8

---
+++ src/main/java/com/anteoy/coreJava/nk/RefMethod.java
@@ -0,0 +1,21 @@
+package com.anteoy.coreJava.nk;
+
+/**
+ * Created by zhoudazhuang on 17-11-22.
+ */
+public class RefMethod {
+    public static void main(String[] args) {
+        Integer a = new Integer(1);
+        Integer b = a;
+        doSomeThing(b);
+        System.out.println(a.intValue());
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
+
+    public static void doSomeThing(Integer integer){
+//        integer = new Integer(2);
+//        integer = 2;
+        integer = Integer.valueOf(2);
+    }
+}

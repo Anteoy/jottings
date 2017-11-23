fcac3a9d9e9c688ab84b6c501d9d41b7a5c6f1ad
anteoy <anteoy@gmail.com>
2017-11-23 13:48:39 +0800

    ++ &&

---
+++ src/main/java/com/anteoy/coreJava/nk/_Test.java
@@ -0,0 +1,18 @@
+package com.anteoy.coreJava.nk;
+
+/**
+ * Created by zhoudazhuang on 17-11-23.
+ */
+public class _Test {
+    public static void main(String[] args) {
+        int x =0,y =0,k =0;
+        for (int z = 0;z <5;z++){
+            if ((++x>2) && (++y>2) && (k++ >2)){
+                x++;
+                ++y;
+                k++;
+            }
+        }
+        System.out.println(x + " " + y + " " + k);
+    }
+}

8569b7eb9160ed57562059cd8ebc042d29031bb5
anteoy <anteoy@gmail.com>
2017-12-06 18:15:55 +0800

    -128

---
+++ src/main/java/com/anteoy/coreJava/nk/Byte128.java
@@ -0,0 +1,12 @@
+package com.anteoy.coreJava.nk;
+
+/**
+ * Created by zhoudazhuang on 17-12-6.
+ */
+public class Byte128 {
+    public static void main(String[] args) {
+        short a = 128;
+        byte b = (byte)a;
+        System.out.println(b);
+    }
+}

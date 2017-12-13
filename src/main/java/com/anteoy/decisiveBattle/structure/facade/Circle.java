534c800fa7582694d678eb9888a4120123da5565
anteoy <anteoy@gmail.com>
2017-12-13 10:00:02 +0800

    add facade

---
+++ src/main/java/com/anteoy/decisiveBattle/structure/facade/Circle.java
@@ -0,0 +1,11 @@
+package com.anteoy.decisiveBattle.structure.facade;
+
+/**
+ * Created by zhoudazhuang on 17-12-13.
+ */
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("draw in circle");
+    }
+}
---
+++ src/main/java/com/anteoy/decisiveBattle/structure/facade/Shape.java
@@ -0,0 +1,9 @@
+package com.anteoy.decisiveBattle.structure.facade;
+
+/**
+ * Created by zhoudazhuang on 17-12-13.
+ * 外观模式,客户端和服务端解耦 客户端和外观类耦合
+ */
+public interface Shape {
+    void draw();
+}

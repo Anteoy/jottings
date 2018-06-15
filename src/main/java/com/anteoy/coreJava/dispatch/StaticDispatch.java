95bacfdc38a98741639ca77c969dd2d2d2e9a247
Anteoy <anteoy@gmail.com>
2018-06-15 23:57:38 +0800

    add dispatch

---
+++ src/main/java/com/anteoy/coreJava/dispatch/StaticDispatch.java
@@ -0,0 +1,29 @@
+package com.anteoy.coreJava.dispatch;
+
+/**
+ * Created by zhoudazhuang on 18-6-15.
+ */
+public class StaticDispatch {
+    static abstract class Human{
+    }
+    static class Man extends Human{
+    }
+    static class Woman extends Human{
+    }
+    public static void sayHello(Human guy){
+        System.out.println("hello,guy!");
+    }
+    public static void sayHello(Man guy){
+        System.out.println("hello,gentlemen!");
+    }
+    public static void sayHello(Woman guy){
+        System.out.println("hello,lady!");
+    }
+
+    public static void main(String[] args) {
+        Human man=new Man();
+        Human woman=new Woman();
+        sayHello(man);
+        sayHello(woman);
+    }
+}

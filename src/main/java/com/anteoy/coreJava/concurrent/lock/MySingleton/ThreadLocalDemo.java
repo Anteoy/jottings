aa32184ea9b629e4d3cfa8e1b35ba082dbf1d2ac
anteoy <anteoy@gmail.com>
2019-03-05 18:33:06 +0800

    update

--- src/main/java/com/anteoy/coreJava/concurrent/lock/MySingleton/ThreadLocalDemo.java
+++ src/main/java/com/anteoy/coreJava/concurrent/lock/MySingleton/ThreadLocalDemo.java
@@ -1,12 +1,19 @@
 package com.anteoy.coreJava.concurrent.lock.MySingleton;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by root on 17-3-19.
  */
 public class ThreadLocalDemo {
-    private static ThreadLocal<String> threadLocal = new ThreadLocal<String>();
+    private ThreadLocal<String> threadLocal = new ThreadLocal<String>();
+    HashMap  hashMap = new HashMap();
+    Hashtable hashtableh = new Hashtable();
+    ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
 
-    static class MyRunnable implements Runnable {
+    class MyRunnable implements Runnable {
         private int num;
 
         public MyRunnable(int num) {
@@ -15,14 +22,17 @@ public class ThreadLocalDemo {
 
         @Override
         public void run() {
+            hashMap.put("1",2);
+            String test = "";
+            hashMap.get("1");
             threadLocal.set(String.valueOf(num));
-            System.out.println("threadLocalValue:" + threadLocal.get());
+            System.out.println("threadLocalValue:"+ Thread.currentThread().getName() + "=>" + threadLocal.get());
         }
     }
 
     public static void main(String[] args) {
-        new Thread(new MyRunnable(1)).start();
-        new Thread(new MyRunnable(2)).start();
-        new Thread(new MyRunnable(3)).start();
+        new Thread(new ThreadLocalDemo().new MyRunnable(1)).start();
+        new Thread(new ThreadLocalDemo().new MyRunnable(2)).start();
+        new Thread(new ThreadLocalDemo().new MyRunnable(3)).start();
     }
 }

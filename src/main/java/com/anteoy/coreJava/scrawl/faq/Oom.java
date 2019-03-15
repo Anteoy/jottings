23bb5c229b82d471611fcb9574a5dc229f8e61ec
anteoy <anteoy@gmail.com>
2019-03-15 18:26:27 +0800

    update

--- src/main/java/com/anteoy/coreJava/scrawl/faq/Oom.java
+++ src/main/java/com/anteoy/coreJava/scrawl/faq/Oom.java
@@ -1,6 +1,5 @@
 package com.anteoy.coreJava.scrawl.faq;
 
-
 import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,39 +7,59 @@ import java.util.List;
 /**
  * @auther zhoudazhuang
  * @date 19-3-13 11:18
- * @description  java -Xms2m -Xmx3m -XX:+PrintGCDetails -cp . com.anteoy.coreJava.scrawl.faq.Oom
+ * @description java -Xms2m -Xmx3m -XX:+PrintGCDetails -cp . com.anteoy.coreJava.scrawl.faq.Oom
  */
 public class Oom {
-    static void StrongReference1() throws InterruptedException {
-        List<SoftReference<byte[]>> list = new ArrayList<>();
-        byte[] buff = null;
-        Thread.sleep(30000);
-        for (int i = 0; i < 10; i++) {
-            buff = new byte[1024*1024];
-            SoftReference<byte[]> sr = new SoftReference<byte[]>(buff);
-            list.add(sr);
-        }
+  static void StrongReference1() throws InterruptedException {
+    List<SoftReference<byte[]>> list = new ArrayList<>();
+    Test test = null;
+//    byte[] bytes = null;
+    Thread.sleep(30000);
     for (int i = 0; i < 10; i++) {
-        byte[] obj = list.get(i).get();
-        System.out.println(obj);
+//      test = new Test();
+      byte[] bytes = new byte[1024*1024];
+      System.out.println("sleep 第: " + i + "次");
+//      Thread.sleep(10000);
+      SoftReference<byte[]> sr = new SoftReference<byte[]>(bytes);
+      list.add(sr);
     }
+    for (int i = 0; i < 10; i++) {
+      byte[] obj = list.get(i).get();
+      System.out.println(obj);
     }
+    System.out.println("ok.....");
+    System.gc();
+    Thread.sleep(30000*2);
+    for (int i = 0; i < 10; i++) {
+      byte[] obj = list.get(i).get();
+      System.out.println(obj);
+    }
+  }
 
-    static void StrongReference2() throws InterruptedException {
-        List<SoftReference<byte[]>> list = new ArrayList<>();
-        Thread.sleep(30000);
-        for (int i = 0; i < 10; i++) {
-            byte[] buff = new byte[1024*1024];
-            SoftReference<byte[]> sr = new SoftReference<byte[]>(buff);
-            list.add(sr);
-        }
-        for (int i = 0; i < 10; i++) {
-            byte[] obj = list.get(i).get();
-            System.out.println(obj);
-        }
+  static void StrongReference2() throws InterruptedException {
+    List<SoftReference<byte[]>> list = new ArrayList<>();
+    Thread.sleep(30000);
+    for (int i = 0; i < 10; i++) {
+      Test test = new Test();
+      System.out.println("sleep 第: " + i + "次");
+      Thread.sleep(10000);
+      SoftReference<byte[]> sr = new SoftReference<byte[]>(test.data);
+      list.add(sr);
     }
+    for (int i = 0; i < 10; i++) {
+      byte[] obj = list.get(i).get();
+      System.out.println(obj);
+    }
+    System.out.println("ok.....");
+    Thread.sleep(30000);
+  }
+
+  static class Test {
+    byte[] data = new byte[1024 * 1024];
+  }
 
   public static void main(String[] args) throws InterruptedException {
-    StrongReference2();
+    StrongReference1();
   }
+
 }
